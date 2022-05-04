06e6522c7010242971302288e9147c71d80b1b40
João Inácio Neto <birobirobiro.dev@gmail.com>
2022-05-04 10:12:55 -0300

    Aula 03 Finalizada :rocket:

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function onScroll() {
   }
 }
 
- 
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -14,3 +13,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
