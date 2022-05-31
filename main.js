d9399f9ddc5b8338708523a79e80054378f56444
Kauan Anjos <63460067+KauanAnjos@users.noreply.github.com>
2022-05-30 22:04:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function activateMenuAtCurrentSection(section) {
 
 function showNavOnScroll() {
   const navigation = document.getElementById("navigation")
-  if (scrollY > 0) {]
+  if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
